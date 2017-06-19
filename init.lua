47e13bbeb7c23860958c75171824251db302251b
Gerolf Seitz <gerolf.seitz@gmail.com>
2017-06-19 21:03:26 +0200

    f17/8 workaround not needed anymore with latest karabiner-elements beta

--- init.lua
+++ init.lua
@@ -11,10 +11,9 @@ bindHyper("Right", push(1/2, 0, 1/2, 1))
 bindHyper("F", push(0, 0, 1, 1))
 
 appShortcut("iTerm", "I")
-appShortcut("HipChat", "H")
+appShortcut("Slack", "S")
 appShortcut("Google Chrome", "G")
-appShortcut("Microsoft Outlook", "O")
-appShortcut("Sublime Text", "S")
+appShortcut("Sublime Text", "T")
 
 
 
--- utils.lua
+++ utils.lua
@@ -20,28 +20,8 @@ function push(x, y, w, h)
   end
 end
 
-k = hs.hotkey.modal.new({}, "F17")
-
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
-
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
-
--- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
-
 function bindHyper(key, func)
-	k:bind({}, key, nil, func)
+	hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, key, func)
 end
 
 function appShortcut(app, key)
